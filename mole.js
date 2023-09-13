e6b45a2d274d1b3f24923ce6510aca774f8f11bd
Susikumar-Pitchaimuthu <118426077+Susikumar-Pitchaimuthu@users.noreply.github.com>
2023-09-13 08:14:50 +0530

    Second Commit

---
+++ mole.js
@@ -0,0 +1,57 @@
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
+let lastHole;
+let timeUp = false;
+let score = 0;
+let missPoint = 0;
+
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log("Ah nah thats the same one bud");
+    return randomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function peep() {
+  const time = randomTime(200, 1000);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    missPoint += 1;
+    console.log(missPoint);
+    if (missPoint - score == 5) {
+      timeUp = true;
+      alert("You are missed your changes. Restart now");
+    }
+    if (!timeUp) peep();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  missPoint = 0;
+  peep();
+
+  setTimeout(() => (timeUp = true), 10000);
+}
+
+function bonk(e) {
+  if (!e.isTrusted) return; // cheater!
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", bonk));
